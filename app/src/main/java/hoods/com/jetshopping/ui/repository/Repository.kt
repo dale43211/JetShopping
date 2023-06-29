fb92d7959ca76dd12c79b8428de16e42ff673062
dale43211 <dale43211@hotmail,com>
2023-06-28 20:05:58 -0400

    Data package complete. About 50 minutes into tutorial

---
+++ app/src/main/java/hoods/com/jetshopping/ui/repository/Repository.kt
@@ -0,0 +1,43 @@
+package hoods.com.jetshopping.ui.repository
+
+import hoods.com.jetshopping.data.room.ItemDao
+import hoods.com.jetshopping.data.room.ListDao
+import hoods.com.jetshopping.data.room.StoreDao
+import hoods.com.jetshopping.data.room.models.Item
+import hoods.com.jetshopping.data.room.models.ShoppingList
+import hoods.com.jetshopping.data.room.models.Store
+
+class Repository(
+    val listDao: ListDao,
+    val itemDao: ItemDao,
+    val storeDao: StoreDao
+) {
+    val store = storeDao.getAllStores()
+    val getItemsWithListAndStore = listDao.getItemsWithStoreAndList()
+
+    fun getItemWithStoreAndList(id: Int) =
+        listDao.getItemWithStoreAndListFilteredById(id)
+
+    fun getItemWithStoreAndListFilteredById(id: Int) =
+        listDao.getItemsWithStoreAndListFilteredById(id)
+
+    suspend fun insertList(shoppingList: ShoppingList){
+        listDao.insertShoppingList(shoppingList)
+    }
+    suspend fun insertStore(store: Store){
+        storeDao.insert(store)
+    }
+    suspend fun insertItem(item: Item){
+        itemDao.insert(item)
+    }
+    suspend fun deleteItem(item: Item){
+        itemDao.delete(item)
+    }
+
+    suspend fun updateItem(item: Item){
+        itemDao.update(item)
+    }
+
+
+
+}   // end class Repository
